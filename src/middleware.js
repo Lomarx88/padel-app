68a656bc6f2e0505d261b73ad74991561fac0bd1
Cosimo17 <cosimo.lomartire88@gmail.com>
2026-03-27 19:30:21 +0100

    ripristinato middleware con secret esplicito

--- src/middleware.js
+++ src/middleware.js
@@ -1,8 +1,11 @@
 import { NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
 
-export async function proxy(request) {
-  const token = await getToken({ req: request });
+export async function middleware(request) {
+  const token = await getToken({ 
+    req: request,
+    secret: process.env.NEXTAUTH_SECRET
+  });
   
   if (!token) {
     const loginUrl = new URL("/login", request.url);
